ee0610190af3f681eecc3b23a4da45e274a147fc
Todd Wolfson <todd@twolfson.com>
2014-04-13 00:50:37 -0700

    Completed test skeletons

--- test/fixture-dir_test.js
+++ test/fixture-dir_test.js
@@ -1,11 +1,43 @@
-var fixture_dir = require('../');
+var FixtureDir = require('../');
 
-describe('fixture-dir', function () {
-  before(function () {
+describe('A FixtureDir', function () {
+  describe('of a nameless folder', function () {
+    it('creates a temporary directory in our namespace', function () {
 
+    });
+
+    describe('when destroyed', function () {
+      it('no longer exists', function () {
+
+      });
+    });
   });
 
-  it('', function () {
+  describe('of a named folder', function () {
+    it('creates a directory under that name', function () {
 
+    });
+  });
+
+  describe('copying another folder', function () {
+    it('creates a directory with the same contents', function () {
+
+    });
+
+    describe('when destroyed', function () {
+      it('no longer exists', function () {
+
+      });
+    });
+  });
+
+  describe('of a named folder that already exists (e.g. bad test/no cleanup)', function () {
+    it('cleans out the directory', function () {
+
+    });
+
+    it('creates a directory under that name', function () {
+
+    });
   });
 });
